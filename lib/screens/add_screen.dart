44c2100203b5ec0f3ccee79be6417bc6385bff3d
Parikshit Gothwal <parikshit.g@gopherslab.com>
2021-07-17 21:37:40 +0530

    feat: removed unwanted code and screens initialized

---
+++ lib/screens/add_screen.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+
+class AddScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Padding(
+        padding: const EdgeInsets.symmetric(horizontal: 28),
+        child: Column(
+          children: [
+            SizedBox(height:50),
+            Text('ADDSCREEN')
+          ],
+        ),
+      ),
+    );
+  }
+}
